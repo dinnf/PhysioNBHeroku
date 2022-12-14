67e0aa9508620b3260a88d3d328c46b984bd15e9
dinnf <finn.denis@gmail.com>
2022-12-14 20:58:20 +1300

    All auth middleware removed

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -18,14 +18,6 @@ const userSchema = new mongoose.Schema(
             trim: true,
             minlength: 8,
         },
-        tokens: [
-            {
-                token: {
-                    type: String,
-                    required: true,
-                },
-            },
-        ],
         firstname:{
             type:String,
             required:true,
@@ -76,44 +68,6 @@ userSchema.methods.toJSON = function () {
     return userObject;
 };
 
-userSchema.methods.generateAuthToken = async function () {
-    const user = this;
-    const token = jwt.sign(
-        { _id: user.id.toString() },
-        process.env.PHYSIOAPP_JWT_SECRET
-    );
-
-    user.tokens = user.tokens.concat({ token });
-    await user.save();
-
-    return token;
-};
-
-userSchema.statics.findByCredentials = async (username, password) => {
-    const user = await User.findOne({ username });
-
-    if (!user) {
-        throw new Error("Unable to Login");
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-
-    if (!isMatch) {
-        throw new Error("Unable to Login");
-    }
-
-    return user;
-};
-
-userSchema.pre("save", async function (next) {
-    const user = this;
-
-    if (user.isModified("password")) {
-        user.password = await bcrypt.hash(user.password, 8);
-    }
-
-    next();
-});
 
 userSchema.pre("remove", async function (next) {
     const user = this;
--- backend/src/routers/note-routers.js
+++ backend/src/routers/note-routers.js
@@ -1,11 +1,10 @@
 const express = require("express");
 
 const Assessment = require("../models/assessment.js");
-const auth = require("../middlewares/auth");
 
 const router = new express.Router();
 
-router.post("/notes", auth, async (req, res) => {
+router.post("/notes", async (req, res) => {
     const assessment = new Assessment({
         ...req.body,
         owner: req.user._id,
@@ -17,7 +16,7 @@ router.post("/notes", auth, async (req, res) => {
         res.status(500).send(e);
     }
 });
-router.get("/notes", auth, async (req, res) => {
+router.get("/notes", async (req, res) => {
     try {
         await req.user.populate("assessments");
         res.send(req.user.assessments);
@@ -37,7 +36,7 @@ router.get("/notes", auth, async (req, res) => {
     }
 });
 */
-router.get("/notes/:id", auth, async (req, res) => {
+router.get("/notes/:id", async (req, res) => {
     try {
         const assessment = await Assessment.findById({ _id: req.params.id });
         if (!assessment) {
@@ -50,7 +49,7 @@ router.get("/notes/:id", auth, async (req, res) => {
 });
 
 // Get a patient with a specific patient's ID
-router.get("/notes/patient/:patientId", auth, async (req, res) => {
+router.get("/notes/patient/:patientId", async (req, res) => {
     try {
         const patientObjectId = `ObjectId('${req.params.patientId}')`
         console.log(`patientobjectId ${patientObjectId}`)
@@ -63,7 +62,7 @@ router.get("/notes/patient/:patientId", auth, async (req, res) => {
         res.status(500).send(e);
     }
 });
-router.delete("/notes/:id", auth, async (req, res) => {
+router.delete("/notes/:id", async (req, res) => {
     try {
         const assessment = await Assessment.findOneAndDelete({ _id: req.params.id });
 
